e6265dbec585327caeaa00686c24049a89b03610
Josh Stuible <shrewdlove@gmail.com>
2018-11-22 10:47:02 -0800

    Reformatted Doc

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,36 @@
 <template>
-  <div>
+<div>
     <NavBar />
-    <nuxt/>
+    <nuxt />
     <Footer />
-  </div>
+</div>
 </template>
 
 <script>
 import NavBar from '~/components/partials/NavBar.vue'
 import Footer from '~/components/partials/Footer.vue'
+
+const consola = require('consola')
+
 export default {
-  components: {NavBar, Footer},
+    components: {
+        NavBar,
+        Footer
+    },
+    data() {
+        return {
+            videos: {}
+        }
+    },
+    mounted() {
+        // const page = this.$nuxt.$children;
+        // this.videos = page.videos;
+        // consola.info(this.$nuxt.$children[1].$children)
+    }
 }
 </script>
-
 
 <style lang="scss">
 @import 'normalize-scss/sass/normalize/_import-now.scss';
 @import '~/assets/scss/layout.scss';
 </style>
-
