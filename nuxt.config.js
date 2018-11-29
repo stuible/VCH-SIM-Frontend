4f46023408aa44eba2b468f57c8f79353c300674
Josh Stuible <shrewdlove@gmail.com>
2018-11-29 15:19:14 -0800

    Added favicons
    Signed-off-by: Josh Stuible <shrewdlove@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,13 +7,21 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt frontend for VCH&apos;s SIM Site' }
+      { hid: 'description', name: 'description', content: 'Nuxt frontend for VCH&apos;s SIM Site' },
+      // { name: 'msapplication-TileColor', content='#ffffff'},
+      { name: 'theme-color', content: '#ffffff'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:300,400' },
       { rel: "stylesheet", href: "https://use.fontawesome.com/releases/v5.2.0/css/all.css", integrity: "sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ",
-      crossorigin:"anonymous"}
+      crossorigin:"anonymous"},
+      {rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png"},
+      {rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png"},
+      {rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png"},
+      {rel: "manifest", href: "/site.webmanifest"},
+      {rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5FE3C4"}
+
     ]
   },
   router: {
