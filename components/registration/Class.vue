4fe4411b39aecd1b280adecce4346e0faa93c681
Josh Stuible <shrewdlove@gmail.com>
2018-11-30 13:35:56 -0800

    Created registration page and calls API
    Signed-off-by: Josh Stuible <shrewdlove@gmail.com>

---
+++ components/registration/Class.vue
@@ -0,0 +1,63 @@
+<template>
+<div class="class">
+    <div class="info">
+        <h2>{{ classInfo.authority }}</h2>
+        <h3 class="duration" v-if="classInfo.duration">{{ classInfo.duration }}</h3>
+        <h5 class="location" v-if="classInfo.location">{{ classInfo.location }}</h5>
+        <h5 class="instructor" v-if="classInfo.instructor">{{ classInfo.instructor }}</h5>
+    </div>
+    <div class="register"><a class="button" :href="classInfo.url" target="_blank">Register</a></div>
+</div>
+</template>
+
+<script>
+export default {
+    props: {
+        classInfo: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.class {
+    margin-bottom: 2em;
+    border-bottom: 2px solid rgb(228, 228, 228);
+    padding-bottom: 1em;
+    display: flex;
+    justify-content: space-between;
+    align-items: stretch;
+}
+
+h2 {
+    margin-block-start: 10px;
+    margin-block-end: 10px;
+    text-transform: uppercase;
+    font-weight: 900;
+}
+
+h3,
+h4,
+h5 {
+    margin-block-start: 10px;
+    margin-block-end: 10px;
+}
+
+.info {
+    display: inline-block;
+}
+.register {
+    // display: inline-block;
+    align-self: center;
+}
+
+.button{
+    background-color: darkgray;
+    text-decoration: none;
+    color: white;
+    padding: 1em 2em;
+    border-radius: 1em;
+}
+</style>
--- pages/registration.vue
+++ pages/registration.vue
@@ -1,17 +1,46 @@
 <template>
-<div>
+<section>
     <div class="container">
-        <h1>Class Registration</h1>
+        <h1 class="title">Class Registration</h1>
+        <class v-for="c in classes" :key="c.id" :classInfo="c.value"> {{c.value.autority}}</class>
     </div>
-</div>
+</section>
 </template>
 
 <script>
-export default {
+import axios from 'axios'
 
+import Class from '~/components/registration/Class.vue'
+
+export default {
+    components: {
+        Class
+    },
+    async asyncData({ env, params }) {
+    const { data } = await axios.post(`${env.cockpit.apiUrl}/singletons/get/Registration?token=${env.cockpit.apiToken}`,
+      JSON.stringify({
+        // filter: { slug: params.slug },
+        // sort: {
+        //   order: 1
+        // }
+        // populate: 1
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+    console.log(data.Class)
+    return {
+      classes: data.Class,
+    };
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-
+.title {
+    margin-bottom: 2em;
+}
 </style>
