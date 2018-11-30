f0bf60dc6e0e99b7cf73c26343ae476fcf9c3c81
Josh Stuible <shrewdlove@gmail.com>
2018-11-30 11:30:33 -0800

    Mobile menu now closes when you click a link or clickoutside the menu
    Signed-off-by: Josh Stuible <shrewdlove@gmail.com>

--- components/partials/NavBar.vue
+++ components/partials/NavBar.vue
@@ -1,203 +1,250 @@
 <template>
-    <nav>
+<nav ref="menu">
     <div class="container">
-      <nuxt-link class="logo" to="/">
-        <img src="~/assets/image/logos/sim-light.svg" draggable="false" alt="Simulation Facilitation Pathways">
+        <nuxt-link class="logo" to="/">
+            <img src="~/assets/image/logos/sim-light.svg" draggable="false" alt="Simulation Facilitation Pathways">
       </nuxt-link>
-      <input type="checkbox" id="menu" class="burger">
-      <label for="menu" class="burger">☰</label>
-      <ul class="menu">
-        <li>
-          <div class="sub-menu-link"><nuxt-link to="/#modules">Modules</nuxt-link>
-            <ul class="sub-menu">
-              <li v-for="v in $store.state.videos" :key="v.id">
-                <nuxt-link :to="'/video/' + v.slug">{{v.title}}</nuxt-link>
-              </li>
+            <input type="checkbox" id="menu" class="burger" v-model="open">
+            <label for="menu" class="burger">☰</label>
+            <ul class="menu">
+                <li>
+                    <div class="sub-menu-link">
+                        <nuxt-link to="/#modules">Modules</nuxt-link>
+                        <ul class="sub-menu">
+                            <li v-for="v in $store.state.videos" :key="v.id">
+                                <nuxt-link :to="'/video/' + v.slug">{{v.title}}</nuxt-link>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <nuxt-link to="/registration">Class Registration</nuxt-link>
+                </li>
+                <li>
+                    <div class="sub-menu-link">Support
+                        <ul class="sub-menu">
+                            <li>Course Objectives</li>
+                            <li><a href="https://www.ssih.org/dictionary" target="_blank">SIM Dictionary</a></li>
+                            <!-- <li>Contact</li> -->
+                        </ul>
+                    </div>
+                </li>
             </ul>
-          </div>
-        </li>
-        <li><nuxt-link to="/registration">Class Registration</nuxt-link></li>
-        <li>
-            <div class="sub-menu-link">Support
-            <ul class="sub-menu">
-              <li>Course Objectives</li>
-              <li><a href="https://www.ssih.org/dictionary" target="_blank">SIM Dictionary</a></li>
-              <!-- <li>Contact</li> -->
-            </ul>
-          </div>
-        </li>
-      </ul>
     </div>
     <div class="overlay"></div>
-  </nav>
+</nav>
 </template>
 
-<style scoped lang="scss">
+<script>
+export default {
+    data() {
+      return{
+        open: false
+      }
+    },
+    mounted() {
+        document.addEventListener("click", (evt) => {
+            const menuEl = this.$refs.menu;
+            let clickedElement = evt.target; // clicked element
+            let targetElement = evt.target;
+
+            do {
+               console.log(clickedElement.tagName.toLowerCase())
+                if (targetElement == menuEl) {
+                    if(clickedElement.tagName.toLowerCase() === "a" && clickedElement !== undefined){
+                      console.log('is a link')
+                      if(this.open) this.open = false
+                    }
+                    // This is a click inside. Do nothing, just return.
+                    console.log('you clicked inside the element')
+                    return;
+                }
+                // Go up the DOM
+                targetElement = targetElement.parentNode;
+            } while (targetElement);
+
+            // This is a click outside.
+            if(this.open) this.open = false
+        });
+    }
+}
+</script>
+
+<style lang="scss" scoped>
 //Top navigation bar
 
 nav {
-  position: fixed;
-  top: 0;
-  width: 100%;
-  height: $navHeight;
-  background-color: $navColour;
-  z-index: 2;
-  box-shadow: 0px 3px 2px -2px rgba(0, 0, 0, 0.26);
+    position: fixed;
+    top: 0;
+    width: 100%;
+    height: $navHeight;
+    background-color: $navColour;
+    z-index: 2;
+    box-shadow: 0px 3px 2px -2px rgba(0, 0, 0, 0.26);
 }
 
 .logo {
-  all: unset;
-  float: left;
-  width: 12em;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  height: $navHeight;
-  cursor: pointer;
+    all: unset;
+    float: left;
+    width: 12em;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    height: $navHeight;
+    cursor: pointer;
 }
 
 .menu.wrapper {
-  float: right;
-  display: flex;
-  justify-content: flex-end;
-  flex-direction: column;
-  height: $navHeight;
+    float: right;
+    display: flex;
+    justify-content: flex-end;
+    flex-direction: column;
+    height: $navHeight;
 
-  a {
-    color: black;
-    display: inline-block;
-    padding-bottom: 10px;
-    border-bottom: 5px #c0640e solid;
-    text-decoration: none;
-    margin-left: 10px;
-  }
+    a {
+        color: black;
+        display: inline-block;
+        padding-bottom: 10px;
+        border-bottom: 5px #c0640e solid;
+        text-decoration: none;
+        margin-left: 10px;
+    }
 }
 
 .menu {
-  list-style: none;
-  display: block;
-  margin: 0;
-  padding: 0;
-  float: right;
+    list-style: none;
+    display: block;
+    margin: 0;
+    padding: 0;
+    float: right;
 }
 
 //always hide hamburger checkbox
 input.burger {
-  display: none;
+    display: none;
 }
 
 @include breakpoint(phablet) {
-  label.burger {
-    display: none;
-  }
-
-  .menu {
-    li {
-      display: inline-block;
-      cursor: pointer;
+    label.burger {
+        display: none;
     }
 
-    > li > a,
-    > li > div {
-      text-decoration: none;
-      color: #404040;
-      font-weight: 500;
-      letter-spacing: 0.02em;
-      line-height: $navHeight;
-      padding: 0 14px;
-      display: block;
-      height: $navHeight - 5px;
+    .menu {
+        li {
+            display: inline-block;
+            cursor: pointer;
+        }
 
-      &:hover {
-        border-bottom: 5px solid $accentTeal;
-      }
+        >li>a,
+        >li>div {
+            text-decoration: none;
+            color: #404040;
+            font-weight: 500;
+            letter-spacing: 0.02em;
+            line-height: $navHeight;
+            padding: 0 14px;
+            display: block;
+            height: $navHeight - 5px;
+
+            &:hover {
+                border-bottom: 5px solid $accentTeal;
+            }
+        }
+
+        ul.sub-menu {
+            position: absolute;
+            display: none;
+            background-color: #fff;
+            padding: 15px 20px;
+            color: #000;
+            margin-left: -14px;
+            border-radius: 0 0 0.5em 0.5em;
+            box-shadow: 0px 2px 2px 2px rgba(0, 0, 0, 0.26);
+
+            li {
+                display: block;
+                margin: 10px 0;
+                line-height: normal;
+            }
+        }
+
+        .sub-menu-link {
+            &:hover ul.sub-menu {
+                display: block;
+            }
+
+            a {
+                text-decoration: none;
+                color: black;
+            }
+        }
     }
-
-    ul.sub-menu {
-      position: absolute;
-      display: none;
-      background-color: #fff;
-      padding: 15px 20px;
-      color: #000;
-      margin-left: -14px;
-      border-radius: 0 0 0.5em 0.5em;
-      box-shadow: 0px 2px 2px 2px rgba(0, 0, 0, 0.26);
-
-      li {
-        display: block;
-        margin: 10px 0;
-        line-height: normal;
-      }
-    }
-
-    .sub-menu-link {
-      &:hover ul.sub-menu {
-        display: block;
-      }
-
-      a {
-        text-decoration: none;
-        color: black;
-      }
-    }
-  }
 }
 
 //Hamburger menu styles that should only be applied when menu is collapsed
 @media (max-width: map-get($breakpoints, phablet) - 1) {
-  label.burger {
-    cursor: pointer;
-    color: $primaryBody;
-    display: block;
-    float: right;
-    margin-top: 25px;
-    font-size: 2em;
-  }
-
-  input.burger:checked + label {
-    color: $accentRed;
-  }
-
-  .menu {
-    top: $navHeight;
-    min-width: 225px;
-    right: -100vw;
-    height: 100%;
-    position: fixed;
-    background-color: #fff;
-    padding: 2em;
-    transition: all 300ms linear;
-
-    //Top Level Menu Items
-    & > li > a,
-    & > li > div {
-      font-size: 2em;
+    label.burger {
+        cursor: pointer;
+        color: $primaryBody;
+        display: block;
+        float: right;
+        margin-top: 25px;
+        font-size: 2em;
     }
 
-    //Second Level Menu Items
-     ul.sub-menu {
-       font-size: 0.5em;
-     }
-
-    li > a,
-    li > div,
-    li > div > a {
-      text-decoration: none;
-      color: $primaryBody;
-      font-weight: 500;
-      letter-spacing: 0.02em;
-      // padding: 0 14px;
-      display: block;
+    input.burger:checked+label {
+        color: $accentRed;
     }
-  }
 
-  /* Toggle Effect */
-  input:checked ~ label {
-    // background-image: url(close.png);
-  }
+    .menu {
+        top: $navHeight;
+        max-width: 225px;
+        width: 100%;
+        opacity: 0;
+        right: -100vw;
+        height: 100%;
+        position: fixed;
+        background-color: #fff;
+        padding: 2em;
+        transition: all 300ms linear;
+        box-shadow: -3px 0px 2px -2px rgba(0, 0, 0, 0.26);
 
-  input:checked ~ .menu {
-    right: 0;
-  }
+        //Top Level Menu Items
+        &>li>a,
+        &>li>div {
+            font-size: 1.5em;
+            // text-transform: uppercase;
+            font-weight: 900;
+            margin-bottom: 1em;
+        }
+
+        //Second Level Menu Items
+        ul.sub-menu {
+            margin-top: 0.5em;
+            font-size: 0.75em;
+            font-weight: 500;
+            list-style-type: none;
+            //  text
+        }
+
+        li>a,
+        li>div,
+        li>div>a {
+            text-decoration: none;
+            color: $primaryBody;
+            letter-spacing: 0.02em;
+            display: block;
+        }
+    }
+
+    /* Toggle Effect */
+    input:checked~label {
+        // background-image: url(close.png);
+    }
+
+    input:checked~.menu {
+        right: 0;
+        opacity: 1;
+    }
 }
 </style>
