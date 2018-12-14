e9bbb6387c83b91a2a21787e843b4002f34512fe
Josh Stuible <shrewdlove@gmail.com>
2018-12-14 12:33:19 -0800

    Corrected spelling mistake
    Signed-off-by: Josh Stuible <shrewdlove@gmail.com>

--- components/video/Reflection.vue
+++ components/video/Reflection.vue
@@ -14,7 +14,7 @@
         <svg class="feedback" id="i-msg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
             <path d="M2 4 L30 4 30 22 16 22 8 29 8 22 2 22 Z" />
         </svg>
-        <div class="feedback" ref="feedbackEl">Select all correct asnwers and click submit to view feedback.</div>
+        <div class="feedback" ref="feedbackEl">Select all correct answers and click submit to view feedback.</div>
     </div>
 </section>
 </template>
