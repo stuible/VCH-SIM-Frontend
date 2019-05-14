28f52ac5a28729205b21d1213b34bb1b44e5cbe8
Josh Stuible <shrewdlove@gmail.com>
2019-05-14 12:44:20 -0700

    Show warning is browser is IE
    Signed-off-by: Josh Stuible <shrewdlove@gmail.com>

---
+++ components/partials/Modal.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="modal-mask">
+    <div class="modal-wrapper">
+      <div class="modal-container">
+        <div class="modal-header">
+          <slot name="header">default header</slot>
+        </div>
+
+        <div class="modal-body">
+          <slot name="body">default body</slot>
+        </div>
+
+        <div class="modal-footer">
+          <slot name="footer">
+            <button class="modal-default-button" @click="$emit('close')">OK</button>
+          </slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 400px;
+  margin: 0px auto;
+  padding: 30px 40px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  font-size: 1.75em;
+  color: $primaryBody;
+}
+
+.modal-body {
+  margin: 20px 0;
+  line-height: 1.25em;
+}
+
+.modal-footer {
+  text-align: center;
+  margin-top: 3em;
+
+  button {
+    background-color: $accentTeal;
+    cursor: pointer;
+    text-decoration: none;
+    color: white;
+    font-weight: 600;
+    padding: 1em 2em;
+    border-radius: 1em;
+    transition: background-color 100ms ease-in;
+    border-style: none;
+
+    &:hover {
+      background-color: $accentTeal;
+    }
+
+    &:focus {
+      outline: none;
+    }
+
+    &:active {
+      // border-style: none;
+    }
+  }
+}
+
+.modal-default-button {
+  //   float: right;
+  margin: 0 auto;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,29 +1,68 @@
 <template>
-<div>
-    <NavBar />
-    <nuxt />
-    <Footer />
-</div>
+  <div>
+    <NavBar/>
+    <modal v-if="showIEWarning" @close="showIEWarning = false">
+        <h3 slot="header">Incompatible Browser</h3>
+        <p slot="body">
+            You are running Internet Explorer, this browser is outdated and will not render parts of this site correctly.
+            For a complete experience, please use a modern browser such as Mozilla Firefox or Google Chrome.
+        </p>
+    </modal>
+    <nuxt/>
+    <Footer/>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
-import NavBar from '~/components/partials/NavBar.vue'
-import Footer from '~/components/partials/Footer.vue'
+import NavBar from "~/components/partials/NavBar.vue";
+import Footer from "~/components/partials/Footer.vue";
+import Modal from "~/components/partials/Modal.vue";
 
 export default {
-    components: {
-        NavBar,
-        Footer
-    },
-    mounted() {
+  components: {
+    NavBar,
+    Footer,
+    Modal
+  },
+  data(){
+      return {
+          showIEWarning: false
+      }
+  },
+  mounted() {
+      if(this.isIE()){
+          this.showIEWarning = true;
+          console.log('IE Detected, show warning');
+      }
+  },
+  methods: {
+    isIE() {
+      var ua = window.navigator.userAgent;
 
-    },
-}
+      var msie = ua.indexOf("MSIE ");
+      if (msie > 0) {
+        // IE 10 or older => return version number
+        return true;
+      }
+
+      var trident = ua.indexOf("Trident/");
+      if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf("rv:");
+        return true;
+      }
+
+      // other browser
+      return false;
+    }
+  }
+};
 </script>
 
 <style lang="scss">
-@import 'normalize-scss/sass/normalize/_import-now.scss';
-@import '~/assets/scss/layout.scss';
+@import "normalize-scss/sass/normalize/_import-now.scss";
+@import "~/assets/scss/layout.scss";
+
 </style>
